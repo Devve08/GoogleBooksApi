0d1c82be4c886d15999d627253a5c0b548e88468
unknown <mhamad.safa.08@gmail.com>
2022-12-19 01:58:48 +0200

    git ignore

---
+++ src/helpers/constants.js
@@ -0,0 +1,5 @@
+export const client_id = "965736435964-2m9s3re66kum09cm8dehah092q93ogq6.apps.googleusercontent.com"
+
+export const apiKey = "AIzaSyAulOF6syrLp2ue48xUBvSg_GrTLm1zddc"
+
+export const baseUrl = 'https://www.googleapis.com/books/v1/volumes'
